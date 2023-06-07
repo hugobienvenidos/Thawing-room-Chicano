e4f9194ddb0d3e14fcde2767998d13922c11cd52
hugobienvenidos <hugo.bienvenidos@gmail.com>
2023-06-07 11:59:18 -0700

    for ensenada

--- src/secrets.h
+++ src/secrets.h
@@ -3,11 +3,14 @@
 // #define SECRET_SSID "GFguestwifi_AP"
 // #define SECRET_SSID "MFPGuestWifi_FTY1"  // replace MySSID with your WiFi network name
 // #define SECRET_PASS "Pr0@#17774$$M@f1"
-#define SECRET_SSID "GFguestwifi_2.4ghz@unifi" 
-#define SECRET_PASS "GF@9300@a"
+// #define SECRET_SSID "GFguestwifi_2.4ghz@unifi" 
+// #define SECRET_PASS "GF@9300@a"
+#define SECRET_SSID "Pez Gordo" 
+#define SECRET_PASS "SardinaMacarena2021"
 #define HOST_NAME "el-thawingnator"
 // #define IP_ADDRESS "192.168.1.160" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
-#define IP_ADDRESS "172.16.30.140" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
+// #define IP_ADDRESS "172.16.30.140" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
+#define IP_ADDRESS "192.168.100.54" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
 
 
 #define PORT      1883
